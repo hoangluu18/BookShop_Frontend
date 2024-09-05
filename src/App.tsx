bcd1472f195f81c58f2d8a90852fb9830e588d89
Lưu Việt Hoàng <luu186835@gmail.com>
2024-09-05 22:48:56 +0700

    chore: build navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './layout/header-footer/Navbar';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
-  return (
-    <div className="App">
-        BOOK STORE APP
-    </div>
-  );
+    return(
+      <div>
+        <Navbar />
+      </div>
+    )
 }
 
 export default App;
---
+++ src/layout/header-footer/Navbar.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';import '@fortawesome/fontawesome-free/css/all.min.css';
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Bookstore</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Trang chủ</a>
+                        </li>
+                        
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Thể loại sách
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Thể loại 1</a></li>
+                                <li><a className="dropdown-item" href="#">Thể loại 2</a></li>
+                                <li><a className="dropdown-item" href="#">Thể loại 3</a></li>
+                            </ul>
+                        </li>
+
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Quy định bán hàng
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Quy định 1</a></li>
+                                <li><a className="dropdown-item" href="#">Quy định 2</a></li>
+                                <li><a className="dropdown-item" href="#">Quy định 3</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" >Liên hệ</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+
+                    <ul className="navbar-nav me-1">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="fas fa-shopping-cart"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul className="navbar-nav me-1">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="fa-solid fa-user"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
